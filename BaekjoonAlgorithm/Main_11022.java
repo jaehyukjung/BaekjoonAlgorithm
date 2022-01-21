9dc0f7730107e1f918a261dfc674b7beb3a93af3
jaehyukjung <wjdwogur9@naver.com>
2022-01-21 13:21:21 +0900

    단계별 풀이 [01.21] for문 문제 버퍼 사용 방법_11022 숫자 두개 입력받는 방식

---
+++ BaekjoonAlgorithm/Main_11022.java
@@ -0,0 +1,36 @@
+import javax.imageio.IIOException;
+import java.io.*;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] argv) throws IOException {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int caseCount = 0;
+        caseCount = Integer.parseInt(bf.readLine());
+
+        String[] numbersSet = new String[caseCount];
+        int[] numbersSet1 = new int[caseCount];
+        int[] numbersSet2 = new int[caseCount];
+        int[] sum = new int[caseCount];
+
+        for (int i = 0; i < caseCount; i++) {
+            numbersSet[i] = bf.readLine();
+        }
+
+        for (int i = 0; i < caseCount; i++) {
+            int numberA = Integer.parseInt(numbersSet[i].split(" ")[0]);
+            numbersSet1[i]=numberA;
+            int numberB = Integer.parseInt(numbersSet[i].split(" ")[1]);
+            numbersSet2[i]=numberB;
+            sum[i] = numberA + numberB;
+        }
+        for(int i =0;i < caseCount; i++){
+            bw.write("Case #"+String.valueOf(i+1)+":"+String.valueOf(numbersSet1[i])+" + "+String.valueOf(numbersSet2[i])+" = "+String.valueOf(sum[i]));
+            bw.newLine();
+        }
+        bw.flush();
+        bw.close();
+    }
+
+}

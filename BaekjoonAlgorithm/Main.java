6b39f3735492bdd83b853eedb4a4605f3585aecc
jaehyukjung <wjdwogur9@naver.com>
2022-01-21 11:19:28 +0900

    단계별 풀이 [01.21] for문 문제 버퍼 사용 방법

---
+++ BaekjoonAlgorithm/Main.java
@@ -0,0 +1,29 @@
+import javax.imageio.IIOException;
+import java.io.*;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] argv) throws IOException {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int caseCount = 0;
+        caseCount = Integer.parseInt(bf.readLine());
+
+        String[] numbersSet = new String[caseCount];
+
+        for (int i = 0; i < caseCount; i++) {
+            numbersSet[i] = bf.readLine();
+        }
+
+        for (int i = 0; i < caseCount; i++) {
+            int numberA = Integer.parseInt(numbersSet[i].split(" ")[0]);
+            int numberB = Integer.parseInt(numbersSet[i].split(" ")[1]);
+            int sum = numberA + numberB;
+            bw.write(String.valueOf(sum));
+            bw.newLine();
+        }
+        bw.flush();
+        bw.close();
+    }
+
+}

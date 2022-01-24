ce57be0b0114ea7f008427f315df36fcaa0b3001
jaehyukjung <jhdool1594@gmail.com>
2022-01-24 23:14:46 +0900

    [1.24] 단계별 함수 3052번 문제

---
+++ BaekjoonAlgorithm/Baekjoon/Main_3052.java
@@ -0,0 +1,39 @@
+package Baekjoon;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main_3052 {
+    public static void main(String[] argv){
+        Scanner sc = new Scanner(System.in);
+        int arr1[] = new int[10];
+        int arr2[] = new int[10];
+        int arr3[] = new int[42];
+        ArrayList<Integer> arr4 = new ArrayList<>();
+
+        for(int i =0;i<42;i++){
+            arr3[i]=i;
+        }
+        for(int i =0;i<10;i++){
+            arr1[i] = sc.nextInt();
+            arr2[i] = arr1[i]%42;
+        }
+        int cnt=0;
+        for(int i=0;i<10;i++){
+            for(int j=0;j<42;j++){
+                if(arr2[i]==j) arr1[i]=j;
+            }
+        }
+        Arrays.sort(arr1);
+        int num = arr1[0];
+        arr4.add(num);
+        for(int i=1;i<10;i++){
+            if(num !=arr1[i]) {
+                arr4.add(num);
+                num = arr3[i];
+            }
+        }
+        System.out.println(arr4.size());
+    }
+}

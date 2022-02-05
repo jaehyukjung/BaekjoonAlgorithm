40dbec50833a0f10b6be0b6c09c890454166527c
jaehyukjung <jhdool1594@gmail.com>
2022-02-05 19:37:12 +0900

    단계별 풀이 [02.05] 10809번 문제

--- BaekjoonAlgorithm/Main.java
+++ BaekjoonAlgorithm/Main.java
@@ -1,29 +1,28 @@
-import javax.imageio.IIOException;
-import java.io.*;
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] argv) throws IOException {
-        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        int caseCount = 0;
-        caseCount = Integer.parseInt(bf.readLine());
+    public static void main(String[] argv) {
+        Scanner sc = new Scanner(System.in);
+        String str = sc.next();
+        char alp[] =new char[26];
+        int arr[] =new int[26];
 
-        String[] numbersSet = new String[caseCount];
-
-        for (int i = 0; i < caseCount; i++) {
-            numbersSet[i] = bf.readLine();
+        for(int i=0;i<26;i++){
+            alp[i] = (char)('a'+i);
+            arr[i] = -1;
         }
 
-        for (int i = 0; i < caseCount; i++) {
-            int numberA = Integer.parseInt(numbersSet[i].split(" ")[0]);
-            int numberB = Integer.parseInt(numbersSet[i].split(" ")[1]);
-            int sum = numberA + numberB;
-            bw.write(String.valueOf(sum));
-            bw.newLine();
+        for(int i=0;i<str.length();i++){
+            for(int j=0;j<26;j++){
+                if(str.charAt(i)==alp[j] && arr[j]==-1){
+                    arr[j] = i;
+                }
+            }
         }
-        bw.flush();
-        bw.close();
+        for(int i=0;i<26;i++){
+            System.out.print(arr[i]+" ");
+        }
+
     }
 
 }

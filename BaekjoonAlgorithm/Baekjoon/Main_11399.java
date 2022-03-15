91b7510b8ae239ef19c2d72ac5acfe4fbbf77063
jaehyukjung <jhdool1594@gmail.com>
2022-03-15 11:14:17 +0900

    단계별 풀이 [03.13] 11399번 문제

---
+++ BaekjoonAlgorithm/Baekjoon/Main_11399.java
@@ -0,0 +1,25 @@
+package Baekjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main_11399 {
+    public static void main(String[] argv) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int arr[]=new int[N];
+        for(int i=0;i<N;i++){
+            arr[i]=Integer.parseInt(st.nextToken());
+        }
+        Arrays.sort(arr);
+        int sum=0;
+        for(int i=0;i<N;i++){
+            sum = sum+(i+1)*arr[N-1-i];
+        }
+        System.out.println(sum);
+    }
+}

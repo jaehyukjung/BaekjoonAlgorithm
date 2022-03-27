9a37d1a6aee59cebebe783b8a4f61c5a3ea18cb7
jaehyukjung <jhdool1594@gmail.com>
2022-03-27 12:37:16 +0900

    단계별 풀이 [03.27] 11060번 문제 dfs/bfs

--- BaekjoonAlgorithm/Baekjoon/Main_11060.java
+++ BaekjoonAlgorithm/Baekjoon/Main_11060.java
@@ -10,11 +10,26 @@ public class Main_11060 {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(br.readLine());
         int[] arr = new int[N];
+        int[] dp = new int[N];
         StringTokenizer st = new StringTokenizer(br.readLine());
         for(int i=0;i<N;i++){
             arr[i] = Integer.parseInt(st.nextToken());
+            dp[i] =-1;
         }
-        int cnt =0;
+        dp[0]=0;
+        for(int i=0;i<N;i++){
+            if(dp[i]==-1) continue;
+            for(int j=1;j<=arr[i];j++){
+                if(i+j<N){
+                    if(dp[i+j]==-1 || dp[i+j]>dp[i]+1){
+                        dp[i+j] =dp[i]+1;
+                    }
+                }
+            }
+        }
+        System.out.println(dp[N-1]);
+
+        /*int cnt =0;
         int i=0;
         while(i<N-1){
             int max =0;
@@ -27,6 +42,11 @@ public class Main_11060 {
             if( i == N) break;
 
         }
-        System.out.println(cnt);
+        System.out.println(cnt);*/ //여기까지가 내가 푼 풀이 맞데틀..
+
+
+
+
+
     }
 }
